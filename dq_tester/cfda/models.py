fb4cbeba5447e5a0fde7b7c445d6c2de66e7668f
kaitlin <kaitlin@kaitlin-work.(none)>
2010-10-08 17:28:59 -0400

    lots of changes, finished up assistance type and obligation importers

--- dq_tester/cfda/models.py
+++ dq_tester/cfda/models.py
@@ -1,7 +1,29 @@
-from mongoengine import connect, Document, EmbeddedDocument, EmbeddedDocumentField, StringField, IntField, ListField, DictField
+from mongoengine import connect, Document, EmbeddedDocument, EmbeddedDocumentField, StringField, IntField, ListField, DictField, ObjectIdField
 from settings import DATABASE_NAME
-
+import re
 connect(DATABASE_NAME)
+
+
+ASSIST_OPTIONS = (
+        (1, "Formula Grants", re.compile('formula.*grants')),
+        (2, "Project Grants", re.compile('project.*grants|loans\/grants')),
+        (3, "Direct Payments for Specified Use", re.compile('direct.*payments.*specified')),
+        (4, "Direct Payments with Unrestricted Use", re.compile('direct.*payments.*unrestricted')),
+        (5, "Direct Loans", re.compile('direct.*loans')),
+        (6, "Guaranteed/Insured Loans", re.compile('guaran.*loan|loan.*guaran')),
+        (7, "Insurance", re.compile('insur.*')),
+        (8, "Sale, Exchange, or Donation of Property and Goods", re.compile('sale.*exchange')),
+        (9, "Use of Property, Facilities, and Equipment", re.compile('property.*facilit')),
+        (10, "Provision of Specialized Services", re.compile('specialized.*')),
+        (11, "Advisory Services and Counseling", re.compile('advis.*counsel')),
+        (12, "Dissemination of Technical Information", re.compile('dissem.*tech|information')),
+        (13, "Training", re.compile('training')),
+        (14, "Investigation of Complaints", re.compile('investigation')),
+        (15, "Federal Employment", re.compile('employment')),
+        (16, "Cooperative Agreements", re.compile('coop.*agree')),
+    )
+
+
 
 class BudgetNumber(EmbeddedDocument):
     fiscal_year = IntField(required=True)
@@ -29,27 +51,19 @@ class Agency(EmbeddedDocument):
     name = StringField()
     budget_numbers = ListField(EmbeddedDocumentField(BudgetNumber))
 
-
-class FundingType(EmbeddedDocument):
-    code = IntField(required=True)
-    name = StringField()
-
 class Obligation(EmbeddedDocument):
     fiscal_year = IntField(required=True)
     amount = IntField()
-    funding_type = FundingType()
-
-class AssistanceType(EmbeddedDocument):
-    number = IntField(required=True)
-    name = StringField()
+    assistance_type = IntField()
 
 class Program(Document):
-    _id = StringField(required=True, max_length=6)
+    number = StringField(required=True, unique=True)
+    title = StringField()
     description = StringField()
-    funding_types = ListField(EmbeddedDocumentField(FundingType))
     obligations = ListField(EmbeddedDocumentField(Obligation))
-    assistance_types = ListField(EmbeddedDocumentField(AssistanceType))
+    assistance_types = ListField(IntField())
     average_assist = IntField()
     range_assist = ListField(IntField())
     run = StringField()
-
+    
+  
--- dq_tester/loaders/cfda_loader.py
+++ dq_tester/loaders/cfda_loader.py
@@ -1,33 +1,158 @@
 from cfda.models import *
-from settings import PROJECT_ROOT, DATABASE_NAME
+from settings import PROJECT_ROOT
 from django.utils.encoding import smart_unicode
 from mongoengine.base import ValidationError
-from mongoengine import connect
 import csv
+import re
 
-connect(DATABASE_NAME)
+#obligaton re's
+re_funding = re.compile('FY ([0-1][0,1,6-9]{1,1})( est. | est | )[\$]([0-9,]+)')
+re_funding_type = re.compile('\((.*?)\)')
+re_exclude = re.compile('[sS]alaries')
+re_direct = re.compile('payments')
+re_grants = re.compile('grants|award|ship|agreement')
+re_loan = re.compile('[lL]oan')
+re_guar = re.compile('[gG]uarantee')
+re_insur = re.compile('[iI]nsur|indemniti')
+account = re.compile('[\d]{2}[-][\d]{4}[-][\d]{1}[-][\d]{1}[-][\d]{3}')
+
+ob_assist_list = ( 
+                    (re_direct, (3, 4, 7)),
+                    (re_guar, (6,)),
+                    (re_loan, (5,)),
+                    (re_grants, (1, 2, 3, 4)),
+                    (re_insur, (7,))
+                )
+ 
+VERSION = ''
 
 def get_or_create_program(number):
-    prog = Program.objects(_id=number)
+    prog = Program.objects(number=number)
     if prog:
         return prog[0]
     else:
-        return Program(_id=number)
+        return Program(number=number)
 
-def parse_cfda_line(row, version):
+def match_ob_type_with_assistance(program, text):
+    global ob_assist_list
+    for re in ob_assist_list:
+        if re[0].findall(text):
+            for at in program.assistance_types:
+                if at in re[1]:
+                    return at
+
+
+def add_assistance_type(program, code):
+    if not program.assistance_types:
+        program.assistance_types = [code]
+    elif code not in program.assistance_types:    
+        program.assistance_types.append(code)
+    program.save()
+
+def match_assistance(program, text):
+    for type_tuple in ASSIST_OPTIONS:
+        if text == type_tuple[1].lower() or type_tuple[2].findall(text):
+            return type_tuple[0]
+
+def parse_assistance(program, text):
+    try:
+        asst_types = smart_unicode(text).strip('.').split(';')
+        for asst in asst_types:
+            clean_asst = asst.lower().strip().replace("\n", "")
+            code = match_assistance(program, clean_asst)
+            if code:
+                add_assistance_type(program, code)
+            else:
+                print "Assistance type didn't match for %s from program %s" % (text, program.title)
+        
+            #else:
+             #   tester = text + " -----"
+             #   for at in program.assistance_types:
+             #       tester += ASSIST_OPTIONS[at-1][1]
+             #   print tester
+    except Exception, e:
+        print str(e)
+
+    return
+
+def is_financial(program):
+    financial = False
+    if program.assistance_types:
+        for at in program.assistance_types:
+            if at in [1, 2, 3, 4, 5, 6, 7, 8]:
+                return True
+    return False
+
+def parse_obligation(program, ob_str):
+    
+    matches = re_funding.findall(ob_str)
+    type_matches = re_funding_type.findall(ob_str)
+    ob_type = ''
+    curr_type = ''
+    assist_code = ''
+    curr_year = '2006'
+    type_iter = iter(type_matches)
+
+    if type_matches:
+        curr_type = type_iter.next()
+    for pair in matches:
+        year = '20' + pair[0]
+        if year < curr_year:  #if the year sequence has started over, move to the next type
+            try:
+                curr_type = type_iter.next()
+            except StopIteration:
+                pass      #no more types
+        curr_year = year
+        obligation = pair[2].replace(",", "")
+
+        if (curr_type and re_exclude.findall(curr_type)) or not is_financial(program):
+            continue  #leave out salaries and nonassistance stuff
+        if curr_type:
+            curr_type = curr_type.lower()
+            assist_code = match_assistance(program, curr_type)
+            if not assist_code:
+                assist_code = match_ob_type_with_assistance(program, curr_type) # try and match with an assistance type already defined for this program
+            if assist_code: ob_type = assist_code
+        if not assist_code:                
+            try:
+                ob_type = program.assistance_types[0] #default it to first assistance type defined for program
+            except Exception:
+                ob_type = 1  # grants is always our default
+        
+        obligation = Obligation(fiscal_year=year, amount=obligation, assistance_type=ob_type)
+        # check to see if this obligation exists
+        if program.obligations and obligation in program.obligations:
+            if program.run < VERSION:
+                program.obligations.get(fiscal_year=year, assistance_type=ob_type).amount=obligation
+                program.save()
+        elif not program.obligations:
+            program.obligations = [obligation]
+            program.save()
+        else:
+            program.obligations.append(obligation)
+            program.save()
+
+        program.save()
+
+
+def parse_cfda_line(row):
 
     program_title = smart_unicode(row[0], errors='ignore')
     program_number = smart_unicode(row[1], errors='ignore')
+    assistance_type = smart_unicode(row[6], errors='ignore')
     obligations_text = smart_unicode(row[24], errors='ignore')
     account_text = smart_unicode(row[23], errors='ignore')
     range_text = smart_unicode(row[25], errors='ignore')
 
     program  = get_or_create_program(program_number.strip())
     program.title = program_title
-    
+    parse_assistance(program, assistance_type)
+    parse_obligation(program, obligations_text)
+
+#    for i in program.__dict__.keys():
+#        print "%s : %s" % (i, program.__dict__[i])
+    program.run = VERSION
     program.save()
-
-
 
 def load_cfda(file_name):
     try:
@@ -42,11 +167,11 @@ def load_cfda(file_name):
 
         print "csv file not found"
         return
-
-    version = file_name[-9:-4]
+    global VERSION
+    VERSION = file_name[-9:-4]
     reader.next()
     for row in reader:
-        parse_cfda_line(row, version)
+        parse_cfda_line(row)
 
 
 
